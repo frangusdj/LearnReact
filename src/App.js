261b90c036972438faad08476da5582d446df82f
Franklin Mendoza <36805525+frangusdj@users.noreply.github.com>
2020-08-26 21:48:34 -0500

    30 mutando el estado del componente con una funcion

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from 'react'
-import TarjetaFruta from './componentes/TarjetaFruta'
+import React, {Component} from 'react'
+//import TarjetaFruta from './componentes/TarjetaFruta'
 
+class Contador extends Component{
+  state = {
+    video: {
+      title: 'Super Video',
+      likes: 0
+    }
+  }
+  add = () => {
+    this.setState((state) => ({
+      video: {
+        ...state.video,
+        likes: state.video.likes + 1
+      }
+    }))
+  }
+  render(){
+    return(
+      <div>
+        <h1>
+        {this.state.video.title}
+      </h1>
+      <button onClick = {this.add}>
+        Likes: ({this.state.video.likes})
+      </button>
+      </div>
+    )
+  }
+}
 //Arow funcion
 //<!--llamamos a la funcion como una etiqueta-->
 const App = () => (
     <div>
-      <TarjetaFruta name= 'Sandia' price = {2.00}/>
-      <TarjetaFruta name= 'Fresa' price = {3.56}/>
-      <TarjetaFruta name= 'Naranja' price = {1.75}/>
+      <Contador />
     </div>
   ) // solo lo hemos declarado
 
