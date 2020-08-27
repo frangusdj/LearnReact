9ede1008b1834fafaa0cdc8aa3297d48d706b133
Franklin Mendoza <36805525+frangusdj@users.noreply.github.com>
2020-08-27 15:45:01 -0500

    32 manejando eventos de mouse

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, {Component} from 'react'
 
-const Gato = (props) => (
-  <div>
-    <h1>Gato </h1>
-    <pre>
-      {JSON.stringify(props,null,4)}
-    </pre>
-  </div>
-)
-//Usando un spread combianmos otros datos con las props
+const styles = {
+  heigth: '200px',
+  width: '200px',
+  background: 'gold',
+  padding:'1em',
+  boxSizing: 'border-box'
+}
+
 class App extends Component{
+  state = {
+    x: 0,
+    y: 0
+  }
+  manejador = (event) => {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    })
+  }
   render(){
-    const otrosDatos = {
-      raza: 'tropical',
-      peleasNocturnas: 300
-    }
     return(
-      <div>
-          <Gato 
-            name = 'Garfield'
-            age = '2 años'
-            {...otrosDatos}
-          />
+      <div style = {styles}
+      onMouseMove = {this.manejador}>
+        <div>
+          x: {this.state.x}
+          y: {this.state.y}
+        </div>
       </div>
     )
   }
